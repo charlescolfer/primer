25402408434651ae8b3f640f7f0350aabdb622e9
charles colfer <charlescolfer@users.noreply.github.com>
2019-11-15 11:45:09 -0500

    fixing sign-up rspec test

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -17,7 +17,7 @@ feature 'user registers', %Q{
 
     fill_in 'Email', with: 'john@example.com'
     fill_in 'Password:', with: 'password'
-    fill_in 'Password confirmation:', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
     fill_in 'Username', with: "Charles the Butler"
     fill_in 'City', with: "Boston"
     fill_in 'State', with: "MA"
